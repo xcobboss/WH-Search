769ae1d3c7dd574a9cde453865ec953cd602c6e9
xcobboss <69330205+xcobboss@users.noreply.github.com>
2023-10-10 22:20:38 -0700

    Update script.js

--- script.js
+++ script.js
@@ -1,30 +1,39 @@
-let wines = [];
-
-// Fetch wines from JSON file
-fetch('wines.json')
-    .then(response => response.json())
-    .then(data => {
-        wines = data;
-    })
-    .catch(error => console.error('Fetching error:', error));
-
 document.addEventListener('DOMContentLoaded', () => {
     document.getElementById('searchForm').addEventListener('submit', function(event) {
         event.preventDefault();  // Prevents the default form submission
-        search();  // Your function that performs the search
+        search();  // Function that performs the search
     });
-});
 
-document.getElementById('searchInput').addEventListener('input', function() {
-    const query = document.getElementById('searchInput').value;
-    document.querySelector('button').disabled = !query.trim();
-});
+    document.getElementById('searchInput').addEventListener('input', function() {
+        const query = document.getElementById('searchInput').value;
+        document.querySelector('button').disabled = !query.trim();
+    });
 
-document.addEventListener("DOMContentLoaded", function() {
     document.querySelector('.mobile-search-icon').addEventListener('click', function() {
         document.getElementById('searchForm').submit();
     });
+
+    // Fetch wines from JSON file and initialize Fuse
+    fetch('wines.json')
+        .then(response => response.json())
+        .then(data => {
+            wines = data;
+            initializeFuse(wines);
+        })
+        .catch(error => console.error('Fetching error:', error));
 });
+
+let wines = [];
+let fuse;
+
+function initializeFuse(winesData) {
+    const fuseOptions = {
+        keys: ['name'], 
+        threshold: 0.4, // Adjust as per requirement
+        includeScore: true 
+    };
+    fuse = new Fuse(winesData, fuseOptions);
+}
 
 function search() {
     const query = document.getElementById('searchInput').value.toLowerCase();
@@ -51,62 +60,17 @@ function search() {
 }
 
 function searchWines(query) {
-    const queryTokens = query.split(/\s+/);
     const results = [];
+    const fuseResults = fuse.search(query);
 
-    // Exact Match
-    wines.forEach(wine => {
-        const wineLower = wine.name.toLowerCase();
-        const wineTokens = wineLower.split(/\s+/);
-        
-        if (query === wineLower || wineTokens.includes(query)) {
-            results.push({wine, matchType: 'exact match'});
-        }
-    });
-    
-    // Token Match
-    wines.forEach(wine => {
-        const wineTokens = wine.name.toLowerCase().split(/\s+/);
-
-        if (queryTokens.every(token => wineTokens.includes(token))) {
-            if (!results.find(result => result.wine.name === wine.name)) {
-                results.push({wine, matchType: 'token match'});
-            }
-        }
-    });
-
-    // Fuzzy Match
-    wines.forEach(wine => {
-        const wineLower = wine.name.toLowerCase();
-        
-        // Using String Similarity - Simplistic approach for JS
-        const similarity = getSimilarity(query, wineLower);
-        if (similarity > 0.8) {  // Adjust similarity threshold as needed
-            if (!results.find(result => result.wine.name === wine.name)) {
-                results.push({wine, matchType: 'fuzzy match'});
-            }
+    fuseResults.forEach(result => {
+        if (result.score <= 0.4) { 
+            results.push({
+                wine: result.item, 
+                matchType: result.score === 0 ? 'exact match' : 'fuzzy match'
+            });
         }
     });
 
     return results;
-}
-
-// A simplistic string similarity function
-function getSimilarity(str1, str2) {
-    let longer = str1;
-    let shorter = str2;
-    if (str1.length < str2.length) {
-        longer = str2;
-        shorter = str1;
-    }
-    const longerLength = longer.length;
-    if (longerLength === 0) {
-        return 1.0;
-    }
-    return (longerLength - getEditDistance(longer, shorter)) / parseFloat(longerLength);
-}
-
-// A function to get string edit distance using Levenshtein Distance Algorithm
-function getEditDistance(a, b) {
-    //... implementation here ...
 }
