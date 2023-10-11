fbcb45f599d542d21fbb2f576d93e6659f6f1187
xcobboss <69330205+xcobboss@users.noreply.github.com>
2023-10-10 23:40:16 -0700

    Update script.js

--- script.js
+++ script.js
@@ -13,18 +13,21 @@ let fuse;  // Declare fuse variable globally
 
 // Initialize Fuse with wines data
 function initializeFuse() {
-    const fuseOptions = {
-        keys: [
-            {name: 'name', weight: 0.7}, // Weight: 0.7 for name
-            {name: 'name', weight: 0.3, threshold: 0.2} // Weight: 0.3 for strict matching in name
-        ],
-        includeScore: true,
-        threshold: 0.6, // General threshold
-        location: 0,
-        distance: 100,
-        minMatchCharLength: 1,
-        includeMatches: true
-    };
+const fuseOptions = {
+    keys: [
+        { name: 'name', weight: 0.7 },
+        { name: 'name', weight: 0.3 }
+    ],
+    includeScore: true,
+    threshold: 0.4, 
+    location: 0,
+    distance: 100,
+    minMatchCharLength: 3, 
+    shouldSort: true,
+    findAllMatches: true, 
+    includeMatches: true
+};
+
 
     fuse = new Fuse(wines, fuseOptions);
 }
