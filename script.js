5a82cc1dd9e39553f16d168a3cfccd53aba9e9ea
xcobboss <69330205+xcobboss@users.noreply.github.com>
2023-10-10 23:05:26 -0700

    Update script.js

--- script.js
+++ script.js
@@ -1,64 +1,112 @@
+let wines = [];
+
+// Fetch wines from JSON file
+fetch('wines.json')
+    .then(response => response.json())
+    .then(data => {
+        wines = data;
+    })
+    .catch(error => console.error('Fetching error:', error));
+
 document.addEventListener('DOMContentLoaded', () => {
-    let wines = [];
-
-    // Fetch wines from JSON file
-    fetch('wines.json')
-        .then(response => response.json())
-        .then(data => {
-            wines = data;
-            initializeSearch(wines);
-        })
-        .catch(error => console.error('Fetching error:', error));
-
-    function initializeSearch(wines) {
-        const fuseOptions = {
-            keys: ['name'],
-            includeScore: true,
-            threshold: 0.1, 
-            location: 0,
-            distance: 100,
-            minMatchCharLength: 3,
-            limit: 5 
-        };
-
-        const fuse = new Fuse(wines, fuseOptions);
-
-        document.getElementById('searchForm').addEventListener('submit', function (event) {
-            event.preventDefault();
-            search();
-        });
-
-        document.getElementById('searchInput').addEventListener('input', function () {
-            const query = document.getElementById('searchInput').value;
-            document.querySelector('button').disabled = !query.trim();
-        });
-
-        document.querySelector('.mobile-search-icon').addEventListener('click', function () {
-            document.getElementById('searchForm').submit();
-        });
-
-        function search() {
-            const query = document.getElementById('searchInput').value;
-            const resultsDiv = document.getElementById('results');
-            resultsDiv.innerHTML = ""; 
-
-            if (!query) {
-                resultsDiv.innerHTML = "<p>Please enter a search term.</p>";
-                return;
-            }
-
-            const results = fuse.search(query).sort((a, b) => b.score - a.score); // Sorting by score
-
-            if (results.length === 0) {
-                resultsDiv.innerHTML = "<p>No results found.</p>";
-                return;
-            }
-            
-            results.forEach(({ item, score }) => {
-                const resultDiv = document.createElement('div');
-                resultDiv.innerHTML = `<p>${item.name} <em>(score: ${score.toFixed(2)})</em></p>`;
-                resultsDiv.appendChild(resultDiv);
-            });
-        }
-    }
+    document.getElementById('searchForm').addEventListener('submit', function(event) {
+        event.preventDefault();  // Prevents the default form submission
+        search();  // Your function that performs the search
+    });
 });
+
+document.getElementById('searchInput').addEventListener('input', function() {
+    const query = document.getElementById('searchInput').value;
+    document.querySelector('button').disabled = !query.trim();
+});
+
+document.addEventListener("DOMContentLoaded", function() {
+    document.querySelector('.mobile-search-icon').addEventListener('click', function() {
+        document.getElementById('searchForm').submit();
+    });
+});
+
+function search() {
+    const query = document.getElementById('searchInput').value.toLowerCase();
+    const resultsDiv = document.getElementById('results');
+    resultsDiv.innerHTML = ""; 
+
+    if (!query) {
+        resultsDiv.innerHTML = "<p>Please enter a search term.</p>";
+        return;
+    }
+
+    const results = searchWines(query);
+
+    if (results.length === 0) {
+        resultsDiv.innerHTML = "<p>No results found.</p>";
+        return;
+    }
+    
+    results.forEach(({wine, matchType}) => {
+        const resultDiv = document.createElement('div');
+        resultDiv.innerHTML = `<p>${wine.name} <em>(${matchType})</em></p>`;
+        resultsDiv.appendChild(resultDiv);
+    });
+}
+
+function searchWines(query) {
+    const queryTokens = query.split(/\s+/);
+    const results = [];
+
+    // Exact Match
+    wines.forEach(wine => {
+        const wineLower = wine.name.toLowerCase();
+        const wineTokens = wineLower.split(/\s+/);
+        
+        if (query === wineLower || wineTokens.includes(query)) {
+            results.push({wine, matchType: 'exact match'});
+        }
+    });
+    
+    // Token Match
+    wines.forEach(wine => {
+        const wineTokens = wine.name.toLowerCase().split(/\s+/);
+
+        if (queryTokens.every(token => wineTokens.includes(token))) {
+            if (!results.find(result => result.wine.name === wine.name)) {
+                results.push({wine, matchType: 'token match'});
+            }
+        }
+    });
+
+    // Fuzzy Match
+    wines.forEach(wine => {
+        const wineLower = wine.name.toLowerCase();
+        
+        // Using String Similarity - Simplistic approach for JS
+        const similarity = getSimilarity(query, wineLower);
+        if (similarity > 0.8) {  // Adjust similarity threshold as needed
+            if (!results.find(result => result.wine.name === wine.name)) {
+                results.push({wine, matchType: 'fuzzy match'});
+            }
+        }
+    });
+
+    return results;
+}
+
+// A simplistic string similarity function
+function getSimilarity(str1, str2) {
+    let longer = str1;
+    let shorter = str2;
+    if (str1.length < str2.length) {
+        longer = str2;
+        shorter = str1;
+    }
+    const longerLength = longer.length;
+    if (longerLength === 0) {
+        return 1.0;
+    }
+    return (longerLength - getEditDistance(longer, shorter)) / parseFloat(longerLength);
+}
+
+// A function to get string edit distance using Levenshtein Distance Algorithm
+function getEditDistance(a, b) {
+    //... implementation here ...
+}
