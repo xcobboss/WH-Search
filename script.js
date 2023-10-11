a5f5537582abe7798acd3df25e416ce62e59f229
xcobboss <69330205+xcobboss@users.noreply.github.com>
2023-10-10 23:50:34 -0700

    Update script.js

--- script.js
+++ script.js
@@ -5,38 +5,13 @@ fetch('wines.json')
     .then(response => response.json())
     .then(data => {
         wines = data;
-        initializeFuse();
     })
     .catch(error => console.error('Fetching error:', error));
 
-let fuse;  // Declare fuse variable globally
-
-// Initialize Fuse with wines data
-function initializeFuse() {
-const fuseOptions = {
-    keys: [
-        { name: 'name', weight: 0.7 },
-        { name: 'name', weight: 0.3 }
-    ],
-    includeScore: true,
-    threshold: 0.3, // Try making it slightly stricter
-    location: 0, // Start at the beginning of the string
-    distance: 9, // Look for matches in the first 50 characters
-    minMatchCharLength: 3, 
-    shouldSort: true,
-    findAllMatches: true,
-    includeMatches: true
-};
-
-
-    fuse = new Fuse(wines, fuseOptions);
-}
-
-
 document.addEventListener('DOMContentLoaded', () => {
     document.getElementById('searchForm').addEventListener('submit', function(event) {
-        event.preventDefault();  
-        search();
+        event.preventDefault();  // Prevents the default form submission
+        search();  // Your function that performs the search
     });
 });
 
@@ -61,16 +36,77 @@ function search() {
         return;
     }
 
-    const results = fuse.search(query);  // Utilizing Fuse.js for searching
+    const results = searchWines(query);
 
     if (results.length === 0) {
         resultsDiv.innerHTML = "<p>No results found.</p>";
         return;
     }
     
-    results.forEach(result => {
+    results.forEach(({wine, matchType}) => {
         const resultDiv = document.createElement('div');
-        resultDiv.innerHTML = `<p>${result.item.name} <em>(Score: ${result.score.toFixed(2)})</em></p>`;
+        resultDiv.innerHTML = `<p>${wine.name} <em>(${matchType})</em></p>`;
         resultsDiv.appendChild(resultDiv);
     });
+}
+
+function searchWines(query) {
+    const queryTokens = query.split(/\s+/);
+    const results = [];
+
+    // Exact Match
+    wines.forEach(wine => {
+        const wineLower = wine.name.toLowerCase();
+        const wineTokens = wineLower.split(/\s+/);
+        
+        if (query === wineLower || wineTokens.includes(query)) {
+            results.push({wine, matchType: 'exact match'});
+        }
+    });
+    
+    // Token Match
+    wines.forEach(wine => {
+        const wineTokens = wine.name.toLowerCase().split(/\s+/);
+
+        if (queryTokens.every(token => wineTokens.includes(token))) {
+            if (!results.find(result => result.wine.name === wine.name)) {
+                results.push({wine, matchType: 'token match'});
+            }
+        }
+    });
+
+    // Fuzzy Match
+    wines.forEach(wine => {
+        const wineLower = wine.name.toLowerCase();
+        
+        // Using String Similarity - Simplistic approach for JS
+        const similarity = getSimilarity(query, wineLower);
+        if (similarity > 0.1) {  // Adjust similarity threshold as needed
+            if (!results.find(result => result.wine.name === wine.name)) {
+                results.push({wine, matchType: 'fuzzy match'});
+            }
+        }
+    });
+
+    return results;
+}
+
+// A simplistic string similarity function
+function getSimilarity(str1, str2) {
+    let longer = str1;
+    let shorter = str2;
+    if (str1.length < str2.length) {
+        longer = str2;
+        shorter = str1;
+    }
+    const longerLength = longer.length;
+    if (longerLength === 0) {
+        return 1.0;
+    }
+    return (longerLength - getEditDistance(longer, shorter)) / parseFloat(longerLength);
+}
+
+// A function to get string edit distance using Levenshtein Distance Algorithm
+function getEditDistance(a, b) {
+    //... implementation here ...
 }
