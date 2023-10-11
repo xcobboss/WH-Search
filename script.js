244bca9b2e88316001cce0525e339fd58415522c
xcobboss <69330205+xcobboss@users.noreply.github.com>
2023-10-10 22:46:50 -0700

    Update script.js

--- script.js
+++ script.js
@@ -38,6 +38,46 @@ document.addEventListener('DOMContentLoaded', () => {
             document.getElementById('searchForm').submit();
         });
 
+        document.addEventListener('DOMContentLoaded', () => {
+    let wines = [];
+
+    // Fetch wines from JSON file
+    fetch('wines.json')
+        .then(response => response.json())
+        .then(data => {
+            wines = data;
+            initializeSearch(wines);
+        })
+        .catch(error => console.error('Fetching error:', error));
+
+    function initializeSearch(wines) {
+        const fuseOptions = {
+        keys: ['name'],
+        includeScore: true,
+        threshold: 0.2,  // Adjusted threshold to be more strict
+        location: 0,
+        distance: 100,
+        minMatchCharLength: 3,  // Minimum number of characters that should match
+        limit: 5  // Limit results to the best 5 matches.
+        };
+
+        
+        const fuse = new Fuse(wines, fuseOptions);
+
+        document.getElementById('searchForm').addEventListener('submit', function (event) {
+            event.preventDefault(); 
+            search();
+        });
+
+        document.getElementById('searchInput').addEventListener('input', function () {
+            const query = document.getElementById('searchInput').value;
+            document.querySelector('button').disabled = !query.trim();
+        });
+
+        document.querySelector('.mobile-search-icon').addEventListener('click', function () {
+            document.getElementById('searchForm').submit();
+        });
+
         function search() {
             const query = document.getElementById('searchInput').value;
             const resultsDiv = document.getElementById('results');
@@ -63,3 +103,4 @@ document.addEventListener('DOMContentLoaded', () => {
         }
     }
 });
+
